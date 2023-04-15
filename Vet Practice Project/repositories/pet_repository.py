c7d299610eb9b671ce344905ad5091297d5a657f
Beth Lupton <luptonbeth21@gmail.com>
2023-04-15 11:08:37 +0100

    Second commit paddding out some of the files still to add more information.

---
+++ Vet Practice Project/repositories/pet_repository.py
@@ -0,0 +1,29 @@
+from db.run_sql import run_sql
+
+from models.pet import Pet
+from models.vet import Vet
+import repositories.vet_repository as vet_repository
+
+
+def save(pet):
+   pass
+
+
+def select_all():
+    pass
+
+
+
+def select(id):
+    pass
+
+
+def delete_all():
+    pass
+
+def delete(id):
+    pass
+
+
+def update(pet):
+    pass
---
+++ Vet Practice Project/repositories/vet_repository.py
@@ -0,0 +1,30 @@
+from db.run_sql import run_sql
+
+from models.vet import Vet
+from models.pet import Pet
+
+
+def save(vet):
+    pass
+
+
+def select_all():
+    pass
+
+
+def select(id):
+    pass
+
+def delete_all():
+    pass
+
+
+def delete(id):
+    pass
+
+
+def update(vet):
+    pass
+
+def books(vet):
+    pass
